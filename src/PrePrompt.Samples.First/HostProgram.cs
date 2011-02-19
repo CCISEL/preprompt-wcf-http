e53094b0f7aec9ac62597253878f492a99b37a0c
Pedro Felix <pmhsfelix@gmail.com>
2011-02-19 19:44:35 +0000

    added SOAP sample; Addes Samples.Common with AtomMediaTypeEncoder

--- src/PrePrompt.Samples.First/HostProgram.cs
+++ src/PrePrompt.Samples.First/HostProgram.cs
@@ -2,9 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.ServiceModel.Dispatcher;
+using System.ServiceModel.Syndication;
 using System.Text;
 using Microsoft.ServiceModel.Description;
+using Microsoft.ServiceModel.Dispatcher;
 using Microsoft.ServiceModel.Http;
+using PrePROMPT.Samples.Common;
 
 namespace PrePrompt.Samples.First
 {
@@ -12,15 +17,21 @@ namespace PrePrompt.Samples.First
     public class FirstHostConfiguration : HttpHostConfiguration, IProcessorProvider
     {
 
-        public void RegisterRequestProcessorsForOperation(System.ServiceModel.Description.HttpOperationDescription operation, IList<System.ServiceModel.Dispatcher.Processor> processors, MediaTypeProcessorMode mode)
+        public void RegisterRequestProcessorsForOperation(HttpOperationDescription operation, IList<Processor> processors, MediaTypeProcessorMode mode)
         {
             // No request processors
         }
 
-        public void RegisterResponseProcessorsForOperation(System.ServiceModel.Description.HttpOperationDescription operation, IList<System.ServiceModel.Dispatcher.Processor> processors, MediaTypeProcessorMode mode)
+        public void RegisterResponseProcessorsForOperation(HttpOperationDescription operation, IList<Processor> processors, MediaTypeProcessorMode mode)
         {
-            processors.Add(new XmlProcessor(operation, mode));
             processors.Add(new JsonProcessor(operation, mode));
+            processors.Add(new AtomMediaTypeProcessor(operation, mode)
+                .WithFormatter(
+                    (TheService.TimeZoneListModel tzms) => new SyndicationFeed("Time zones", "List of time zones", null,
+                                                                               tzms.Zones.Select(
+                                                                                   tzm =>
+                                                                                   new SyndicationItem(tzm.Id, tzm.Name,
+                                                                                                       new Uri(tzm.Uri))))));
         }
     }
 
@@ -30,12 +41,13 @@ namespace PrePrompt.Samples.First
         {
             using(var host = new WebHttpServiceHost(typeof(TheService)))
             {
-                var ep = host.AddServiceEndpoint(typeof(TheService), new HttpMessageBinding(), "Http://localhost:8080/first/");
+                var ep = host.AddServiceEndpoint(typeof(TheService), new HttpMessageBinding(), "http://localhost:8080/first/");
                 ep.Behaviors.Add(new HttpEndpointBehavior(new FirstHostConfiguration()));
                 host.Open();
                 Console.WriteLine("host is opened at {0}, press any key to continue", ep.Address);
                 Console.ReadLine();
             }
+
         }
     }
 }
---
+++ src/PrePrompt.Samples.SoapModel/SoapService.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.Text;
+
+namespace PrePrompt.Samples.SoapModel
+{
+    [DataContract(Namespace="http://www.cc.isel.ipl.pt")]
+    public class GetTimeRequest
+    {
+        [DataMember]
+        public String Zone { get; set; }
+    }
+
+    [DataContract(Namespace = "http://www.cc.isel.ipl.pt")]
+    public class GetTimeResponse
+    {
+        [DataMember]
+        public String Time { get; set; }
+    }
+
+    [ServiceContract(Name="TheService",Namespace = "http://www.cc.isel.ipl.pt/")]
+    public interface ITheServiceContract
+    {
+        [OperationContract]
+        GetTimeResponse GetTime(GetTimeRequest req);
+    }
+
+    class TheServiceImpl : ITheServiceContract
+    {
+        public GetTimeResponse GetTime(GetTimeRequest req)
+        {
+            if (req.Zone == null)
+            {
+                return new GetTimeResponse() { Time = DateTime.Now.ToLongTimeString() };
+            }
+            try
+            {
+                var zoneInfo = TimeZoneInfo.FindSystemTimeZoneById(req.Zone);
+                return new GetTimeResponse()
+                           {
+                               Time = TimeZoneInfo.ConvertTime(DateTime.Now, zoneInfo).ToLongTimeString()
+                           };
+            }
+            catch (TimeZoneNotFoundException e)
+            {
+                throw new FaultException(e.Message);
+            }
+        }
+    }
+
+    class HostProgram
+    {
+        static void Main(string[] args)
+        {
+            using(var host = new ServiceHost(typeof(TheServiceImpl)))
+            {
+                host.AddServiceEndpoint(
+                    typeof (ITheServiceContract),
+                    new BasicHttpBinding(),
+                    "http://localhost:8080/soap/time");
+
+                host.Description.Behaviors.Add(new ServiceMetadataBehavior
+                                                   {
+                                                       HttpGetEnabled = true,
+                                                       HttpGetUrl = new Uri("http://localhost:8080/soap/time/wsdl")
+                                                   });
+
+                host.Open();
+                Console.WriteLine("Service is open, press any key to end");
+                TheClient.Run();
+                Console.ReadKey();
+            }
+        }
+    }
+
+    class TheClient
+    {
+        public static void Run()
+        {
+            using(var cf = new ChannelFactory<ITheServiceContract>(new BasicHttpBinding(), "http://localhost:8080/soap/time"))
+            {
+                Console.WriteLine(cf.CreateChannel().GetTime(new GetTimeRequest()).Time);
+            }
+        }
+    }
+
+}
