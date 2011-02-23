6cafb2f46ea4f2cd819418f263490cb332717f5e
Pedro Felix <pmhsfelix@gmail.com>
2011-02-23 16:11:17 +0000

    created second sample; some refactorings

--- src/PrePrompt.Samples.Common/WaveFromTextMediaProcessor.cs
+++ src/PrePrompt.Samples.Common/WaveFromTextMediaProcessor.cs
@@ -2,30 +2,36 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.ServiceModel.Description;
+using System.Speech.Synthesis;
 using System.Text;
 using Microsoft.ServiceModel.Http;
 using PrePrompt.Samples.Common;
 
-namespace PrePrompt.Samples.First
+namespace PrePrompt.Samples.Common
 {
-    class ImageFromTextMediaProcessor : MediaTypeProcessor
+    public class WaveFromTextMediaProcessor : MediaTypeProcessor
     {
 
-        public ImageFromTextMediaProcessor(HttpOperationDescription operation, MediaTypeProcessorMode mode)
+        public WaveFromTextMediaProcessor(HttpOperationDescription operation, MediaTypeProcessorMode mode)
             :base(operation,mode)
         {
         }
 
         public override IEnumerable<string> SupportedMediaTypes
         {
-            get { yield return "image/jpeg"; }
+            get { yield return "audio/x-wav"; }
         }
 
         public override void WriteToStream(object instance, System.IO.Stream stream, System.Net.Http.HttpRequestMessage request)
         {
             var text = instance as string;
             if (text == null) return;
-            ImageTools.WriteJpegCreatedFrom(text, stream);
+            using(var synth = new SpeechSynthesizer())
+            {
+                synth.SetOutputToWaveStream(stream);
+                //synth.Rate -= 10;
+                synth.Speak("current time is "+text);
+            }
         }
 
         public override object ReadFromStream(System.IO.Stream stream, System.Net.Http.HttpRequestMessage request)
---
+++ src/PrePrompt.Samples.Second/HostProgram.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.ServiceModel.Dispatcher;
+using System.ServiceModel.Syndication;
+using Microsoft.ServiceModel.Description;
+using Microsoft.ServiceModel.Http;
+using PrePrompt.Samples.Common;
+using PrePROMPT.Samples.Common;
+
+namespace PrePrompt.Samples.Second
+{
+
+    class HostProgram
+    {   
+        static void Main(string[] args)
+        {
+            var provider = new ProcessorProviderFor<TheService>();
+            provider.RemoveAllMediaTypeProcessors().ForResponses.OfAllOperations();
+
+            
+            provider.Use((o, l, m) => new ImageFromTextMediaProcessor(o, m))
+                .ForResponses.OfOperation(
+                    s => s.GetTimeWithConneg(default(HttpRequestMessage), default(HttpResponseMessage)));
+
+            provider.Use((o, l, m) => new WaveFromTextMediaProcessor(o, m))
+                .ForResponses.OfOperation(
+                    s => s.GetTimeWithConneg(default(HttpRequestMessage), default(HttpResponseMessage)));
+
+            provider.Use((o, l, m) => 
+                    new AtomMediaTypeProcessor(o, m)
+                    .WithFormatter(
+                        (TheService.TimeZoneListModel tzms) => new SyndicationFeed("Time zones", "List of time zones", null,
+                                                                               tzms.Zones.Select(
+                                                                                   tzm =>
+                                                                                   new SyndicationItem(tzm.Id, tzm.Name,
+                                                                                                       new Uri(tzm.Uri)))))
+                )
+                .ForResponses.OfOperation(
+                    s => s.GetZones(default(HttpRequestMessage), default(HttpResponseMessage)));
+
+            provider.Use((o,l,m) => new DataValidationProcessor(o)).ForRequests.OfAllOperations();
+            provider.Use((o,l,m) => new RequestLoggingProcessor(o)).ForRequests.OfAllOperations();
+
+            var config = new HttpHostConfiguration().SetProcessorProvider(provider);
+            
+            using(var host = new WebHttpServiceHost(typeof(TheService)))
+            {
+                var ep = host.AddServiceEndpoint(typeof(TheService), new HttpMessageBinding(){TransferMode = TransferMode.Streamed}, "http://localhost:8080/first/");
+                ep.Behaviors.Add(new HttpEndpointBehavior(config));
+                host.Open();
+                Console.WriteLine("host is opened at {0}, press any key to continue", ep.Address);
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
